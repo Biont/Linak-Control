2ad1dda89e74aee32791000b66e203d67c004dc7
biont <arbelzapf@gmail.com>
2017-04-24 20:15:26 +0200

    implement editing of schedule items

--- src/js/backbone/Views/ScheduleItemView.js
+++ src/js/backbone/Views/ScheduleItemView.js
@@ -1,50 +1,62 @@
 import BiontView from "./BiontView.js";
 import ScheduleFormView from "./ScheduleFormView";
 
-export default class ScheduleItemView extends BiontView.extend( {
-	events: {
-		'click [data-action]'       : 'onButtonClick',
-		'click [data-action="save"]': 'save'
-	}
-} ) {
-	constructor( data, options ) {
-		super( data, options );
-		this.form = new ScheduleFormView( {
-			model: data.model
-		} );
-		this.listenTo( this.model, 'destroy', this.remove );
-	}
+export default class ScheduleItemView extends BiontView.extend({
+    events: {
+        'click [data-action]': 'onButtonClick',
+        'click [data-action="save"]': 'save',
+        'change input': 'onChange'
+    }
+}) {
+    constructor(data, options) {
+        super(data, options);
+        this.form = new ScheduleFormView({
+            model: data.model
+        });
+        this.listenTo(this.model, 'destroy', this.remove);
+    }
 
-	onButtonClick( e ) {
-		e.preventDefault();
-		e.stopImmediatePropagation();
-		let action = jQuery( e.currentTarget ).data( 'action' );
-		switch ( action ) {
-			case'save':
-				this.save();
-				break;
-			case'remove':
-				console.log('isNew',this.model.isNew());
-				this.model.destroy();
-		}
-	}
+    onChange(e) {
+        let $input = jQuery(e.target);
+        switch ($input.attr('type')) {
+            case'time':
+                this.model.set('time', $input.val());
+                break;
+            case'range':
+                this.model.set('height', $input.val());
+        }
+    }
 
-	save() {
-		this.model.save();
-	}
+    onButtonClick(e) {
+        e.preventDefault();
+        e.stopImmediatePropagation();
+        let action = jQuery(e.currentTarget).data('action');
+        switch (action) {
+            case'save':
+                this.save();
+                break;
+            case'remove':
+                console.log('isNew', this.model.isNew());
+                this.model.destroy();
+        }
+    }
 
-	/**
-	 * Slides up and then kills the view
-	 */
-	remove() {
-		this.$el.slideUp(275, () => {
-			super.remove()
-		});
-	}
+    save() {
+        this.model.save();
+    }
 
-	render() {
-		super.render();
-		this.assign( this.form, '[data-schedule-form]' );
-		return this;
-	}
+    /**
+     * Slides up and then kills the view
+     */
+    remove() {
+        this.$el.slideUp(275, () => {
+            super.remove()
+        });
+    }
+
+    render() {
+        super.render();
+        this.assign(this.form, '[data-schedule-form]');
+        return this;
+    }
 }
