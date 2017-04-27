b0eff28188e2a49dafc6fa5e26fcc9d3dcf3c374
biont <arbelzapf@gmail.com>
2017-04-27 21:01:42 +0200

    Fix an error

--- app/util/linakUtil.js
+++ app/util/linakUtil.js
@@ -7,58 +7,54 @@ var _child_process = require('child_process');
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var LinakUtil = function () {
-	/**
-  * Configure the sudo prompt
-  */
-	function LinakUtil() {
-		_classCallCheck(this, LinakUtil);
+    /**
+     * Configure the sudo prompt
+     */
+    function LinakUtil() {
+        _classCallCheck(this, LinakUtil);
+    }
 
-		this.options = {
-			name: 'Electron',
-			icns: '/Applications/Electron.app/Contents/Resources/Electron.icns' };
-	}
-
-	/**
-  * Execute the elevated call to the USB driver
-  *
-  * @param position
-  * @param callback
-  */
+    /**
+     * Execute the elevated call to the USB driver
+     *
+     * @param position
+     * @param callback
+     */
 
 
-	LinakUtil.prototype.moveTo = function moveTo() {
-		var position = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
-		var callback = arguments[1];
+    LinakUtil.prototype.moveTo = function moveTo() {
+        var _arguments = arguments;
+        var position = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 0;
+        var callback = arguments[1];
 
-		console.log('Attempting to move the table to position ' + position);
+        console.log('Attempting to move the table to position ' + position);
 
-		(0, _child_process.exec)(__dirname + '/bin/example-moveTo ' + position, function (error, stdout, stderr) {
-			console.log(arguments);
-			if (error) {
-				console.log(stderr);
-			}
-			this.busy = false;
-			callback(error, stdout, stderr);
-		});
-	};
+        (0, _child_process.exec)(__dirname + '/bin/example-moveTo ' + position, function (error, stdout, stderr) {
+            console.log(_arguments);
+            if (error) {
+                console.log(stderr);
+            }
+            callback(error, stdout, stderr);
+        });
+    };
 
-	LinakUtil.prototype.getHeight = function getHeight(callback) {
-		(0, _child_process.exec)(__dirname + '/bin/example-getHeight', function (error, stdout, stderr) {
-			if (error) {
-				console.error('exec error: ' + error);
-			}
-			var parts = stdout.split(' ');
-			var data = {
-				signal: parts[2],
-				cm: parts[3],
-				raw: stdout
-			};
+    LinakUtil.prototype.getHeight = function getHeight(callback) {
+        (0, _child_process.exec)(__dirname + '/bin/example-getHeight', function (error, stdout, stderr) {
+            if (error) {
+                console.error('exec error: ' + error);
+            }
+            var parts = stdout.split(' ');
+            var data = {
+                signal: parts[2],
+                cm: parts[3],
+                raw: stdout
+            };
 
-			callback(error, data);
-		});
-	};
+            callback(error, data);
+        });
+    };
 
-	return LinakUtil;
+    return LinakUtil;
 }();
 
 exports.default = LinakUtil;
--- src/js/util/linakUtil.js
+++ src/js/util/linakUtil.js
@@ -1,50 +1,44 @@
 import {exec} from "child_process";
 export default class LinakUtil {
-	/**
-	 * Configure the sudo prompt
-	 */
-	constructor() {
+    /**
+     * Configure the sudo prompt
+     */
+    constructor() {
 
-		this.options = {
-			name: 'Electron',
-			icns: '/Applications/Electron.app/Contents/Resources/Electron.icns', // (optional)
-		};
+    }
 
-	}
+    /**
+     * Execute the elevated call to the USB driver
+     *
+     * @param position
+     * @param callback
+     */
+    moveTo(position = 0, callback) {
+        console.log('Attempting to move the table to position ' + position);
 
-	/**
-	 * Execute the elevated call to the USB driver
-	 *
-	 * @param position
-	 * @param callback
-	 */
-	moveTo( position = 0, callback ) {
-		console.log( 'Attempting to move the table to position ' + position );
+        exec(__dirname + '/bin/example-moveTo ' + position, (error, stdout, stderr) => {
+            console.log(arguments);
+            if (error) {
+                console.log(stderr);
+            }
+            callback(error, stdout, stderr)
+        });
+    }
 
-		exec( __dirname + '/bin/example-moveTo ' + position, function( error, stdout, stderr ) {
-			console.log( arguments );
-			if ( error ) {
-				console.log( stderr );
-			}
-			this.busy=false;
-			callback( error, stdout, stderr )
-		} );
-	}
+    getHeight(callback) {
+        exec(__dirname + '/bin/example-getHeight', (error, stdout, stderr) => {
+            if (error) {
+                console.error(`exec error: ${error}`);
+            }
+            let parts = stdout.split(' ');
+            let data = {
+                signal: parts[2],
+                cm: parts[3],
+                raw: stdout
+            };
 
-	getHeight( callback ) {
-		exec( __dirname + '/bin/example-getHeight', ( error, stdout, stderr ) => {
-			if ( error ) {
-				console.error( `exec error: ${error}` );
-			}
-			let parts = stdout.split( ' ' );
-			let data = {
-				signal: parts[ 2 ],
-				cm    : parts[ 3 ],
-				raw   : stdout
-			};
-
-			callback( error, data )
-		} );
-	}
+            callback(error, data)
+        });
+    }
 
 }
