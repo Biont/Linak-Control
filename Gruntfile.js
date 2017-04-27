e0138bdbd120d40da0323d9014e43217f6cb46f5
biont <arbelzapf@gmail.com>
2017-04-27 22:20:19 +0200

    Add packaging

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,43 +1,55 @@
-module.exports = function( grunt ) {
-	grunt.initConfig( {
-		config: {
-			scripts: {
-				src : 'src/js/',
-				dest: 'app/'
-			}
-		},
-		babel : {
-			options: {
-				sourceMap: true,
-			},
-			dist   : {
-				files: [
-					{
-						expand: true,
-						cwd   : '<%= config.scripts.src %>',
-						src   : [ '**/*.js' ],
-						dest  : '<%= config.scripts.dest %>'
-					}
-				]
-			}
+module.exports = function (grunt) {
+    grunt.initConfig({
+        config: {
+            scripts: {
+                src: 'src/js/',
+                dest: 'app/'
+            }
+        },
+        babel: {
+            options: {
+                sourceMap: true,
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: '<%= config.scripts.src %>',
+                        src: ['**/*.js'],
+                        dest: '<%= config.scripts.dest %>'
+                    }
+                ]
+            }
 
-		},
-		watch : {
-			options: {
-				spawn: false
-			},
-			scripts: {
-				files: [
-					'.eslintrc',
-					'<%= config.scripts.src %>**/*.js'
-				],
-				tasks: [
-					'babel'
-				]
-			}
-		}
-	} );
-	require( 'load-grunt-tasks' )( grunt ); // npm install --save-dev load-grunt-tasks
+        },
+        watch: {
+            options: {
+                spawn: false
+            },
+            scripts: {
+                files: [
+                    '.eslintrc',
+                    '<%= config.scripts.src %>**/*.js'
+                ],
+                tasks: [
+                    'babel'
+                ]
+            }
+        },
+        electron: {
+            linux: {
+                options: {
+                    name: 'LinakControl',
+                    dir: '.',
+                    out: 'dist',
+                    platform: 'linux',
+                    arch: 'x64'
+                }
+            }
+        }
+    });
+    require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
 
-	grunt.registerTask( 'default', [ 'babel' ] );
+    grunt.registerTask('default', ['babel']);
+    grunt.registerTask('electron', ['electron']);
 };
