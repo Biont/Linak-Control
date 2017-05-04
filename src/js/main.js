cdab24d7446fdba2ab8b48ffdab018cdafecb58c
Moritz Meißelbach <m.meisselbach@inpsyde.com>
2017-05-04 12:25:07 +0200

    Force overlay scrollbars

--- src/js/main.js
+++ src/js/main.js
@@ -84,7 +84,13 @@ class Main {
 		}
 
 		// Create the browser window.
-		win = new BrowserWindow( { width: 800, height: 600 } );
+		win = new BrowserWindow( {
+			width         : 800,
+			height        : 600,
+			webPreferences: {
+				blinkFeatures: 'OverlayScrollbars'
+			}
+		} );
 
 		// and load the index.html of the app.
 		win.loadURL( url.format( {
