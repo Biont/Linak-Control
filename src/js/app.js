af4bc5d0892e56f8b3103713b4fc74c23d66ab51
biont <arbelzapf@gmail.com>
2017-05-06 22:27:08 +0200

    Add frontend for an autostart setting.

--- src/js/app.js
+++ src/js/app.js
@@ -6,39 +6,40 @@ import ScheduleItem from "./backbone/Models/ScheduleItem";
 
 class App {
 
-	init() {
-		this.listenToNotifications();
-		this.settings = new AppSettingsModel( {
-			id              : 'mainApp',
-			heightOffset    : 62.5,
-			maxHeight       : 6449,
-			enableStatistics: true
-		} );
-		this.settings.fetch( {
-			success: () => {
-				appView.render();
-			}
-		} );
-		let schedule = new ScheduleCollection( [], {
-			model     : ScheduleItem,
-			comparator: function( m ) {
-				return m.getTimeStamp();
-			}
-		} );
-		let appView = new AppView( {
-			el        : '#main-app',
-			settings  : this.settings,
-			collection: schedule
-		} );
-		schedule.fetch();
-	}
+    init() {
+        this.listenToNotifications();
+        this.settings = new AppSettingsModel({
+            id: 'mainApp',
+            heightOffset: 62.5,
+            maxHeight: 6449,
+            autoStart: false,
+            enableStatistics: true
+        });
+        this.settings.fetch({
+            success: () => {
+                appView.render();
+            }
+        });
+        let schedule = new ScheduleCollection([], {
+            model: ScheduleItem,
+            comparator: function (m) {
+                return m.getTimeStamp();
+            }
+        });
+        let appView = new AppView({
+            el: '#main-app',
+            settings: this.settings,
+            collection: schedule
+        });
+        schedule.fetch();
+    }
 
-	listenToNotifications() {
-		background.subscribe( 'subscribe-notifications', ( event, data ) => {
-			let notification = new Notification( 'Linak Control', {
-				body: data.message
-			} );
-		} );
-	}
+    listenToNotifications() {
+        background.subscribe('subscribe-notifications', (event, data) => {
+            let notification = new Notification('Linak Control', {
+                body: data.message
+            });
+        });
+    }
 }
 module.exports = App;
